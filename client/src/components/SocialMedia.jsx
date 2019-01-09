f4c0e58a4e9fd1507d71587f1b34d847b495e2ee
Casey Shamey <caseyshamey@gmail.com>
2019-01-08 23:22:21 -0500

    Test pull request

---
+++ client/src/components/SocialMedia.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SocialMedia = props => {
+  return (
+    <div>
+      <ahref>facebook icon</ahref>
+      <ahref>twitter icon</ahref>
+      <ahref>mail icon</ahref>
+    </div>
+  );
+};
+
+export default SocialMedia;
