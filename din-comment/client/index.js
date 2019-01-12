0c5f0c43d84df617704a4fad0395b82653b39aec
deanlee805 <deanlee805@gmail.com>
2019-01-12 12:00:07 -0800

    MVP comment in proxy

--- din-comment/client/index.js
+++
@@ -1,8 +0,0 @@
-import App from "./components/App.jsx";
-import React, {
-  Component
-} from "react";
-import ReactDOM from "react-dom";
-
-// ReactDOM.render( < App / > , document.getElementById("root"));
-window.App = App;
